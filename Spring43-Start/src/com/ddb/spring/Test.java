026c0b953c2680dbd2189c97f5f0a6d3b325d33e
dbdu <1797890817@qq.com>
2017-01-01 20:21:21 +0800

    add Spring Start use 4.3.3 version

---
+++ Spring43-Start/src/com/ddb/spring/Test.java
@@ -0,0 +1,33 @@
+package com.ddb.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	private static ApplicationContext ctx;
+
+	private static void print(Object object){
+		System.out.println(object);
+	}
+
+	public static void test1() {
+		Users users = new Users();
+		users.setName("Teacher");
+		users.setAddress("hefei");
+		users.setMemo("This is my first Spring project.");
+		print(users);
+	
+	}
+	public static void test2() {
+		ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//通过ApplicationContext的getBean()方法，根据id来获取bean实例
+		Users users = (Users)ctx.getBean("users");
+		print(users);
+	}
+	
+	public static void main(String[] args) {
+		test1();
+		test2();
+	}
+
+}
