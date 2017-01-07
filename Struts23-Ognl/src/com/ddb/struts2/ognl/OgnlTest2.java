568a38c2cc5fc062569a489646c73e432f1aafd1
dbdu <1797890817@qq.com>
2017-01-07 21:43:03 +0800

    update Struts23-ognl to 2.3.30 version

--- Struts23-Ognl/src/com/ddb/struts2/ognl/OgnlTest2.java
+++ Struts23-Ognl/src/com/ddb/struts2/ognl/OgnlTest2.java
@@ -22,7 +22,7 @@ public class OgnlTest2 {
 		OgnlContext context = new OgnlContext();
 		context.put("bean", bean);
 		/**
-		 * ����
+		 * 
 		 */
 		System.out.println(Ognl.getValue("'Hello!'", context));
 		System.out.println(Ognl.getValue("'A'", context));
@@ -30,38 +30,38 @@ public class OgnlTest2 {
 		System.out.println(Ognl.getValue("true", context));
 		System.out.println(Ognl.getValue("null", context));
 		/**
-		 * ����
+		 * 
 		 */
 		System.out.println(Ognl.getValue("#name='zhangsan'", context));
 		/**
-		 * ������ͨ����
+		 * 
 		 */
 		System.out.println(Ognl.getValue("bean.property", context));
 		/**
-		 * ������ͨ����
+		 * 
 		 */
 		System.out.println(Ognl.getValue("bean.method()", context));
 		/**
-		 * ���ʾ�̬���Է���
+		 * 
 		 */
 		System.out.println(Ognl.getValue("@com.ddb.struts2.ognl.OgnlBean@staticProperty", context));
 		System.out.println(Ognl.getValue("@com.ddb.struts2.ognl.OgnlBean@staticMethod()", context));
 		/**
-		 * ���ʹ��췽��
+		 * 
 		 */
 		System.out.println(Ognl.getValue("(new java.lang.String(\"Instance a new object!\")).toString()", context));
 		/**
-		 * ��������Ԫ��
+		 * 获取Bean中数组的元素
 		 */
 		System.out.println(Ognl.getValue("bean.getArray()[0]", context));
 		System.out.println(Ognl.getValue("bean.array[0]", context));
 		/**
-		 * ����ListԪ��
+		 * List
 		 */
 		System.out.println(Ognl.getValue("bean.getList()[0]", context));
 		System.out.println(Ognl.getValue("bean.list[0]", context));
 		/**
-		 * ����MapԪ��
+		 * Map
 		 */
 		System.out.println(Ognl.getValue("bean.getMap()['home']", context));
 		System.out.println(Ognl.getValue("bean.map['home']", context));
@@ -69,7 +69,7 @@ public class OgnlTest2 {
 		System.out.println(Ognl.getValue("bean.map.office", context));
 		
 		/**
-		 * �������
+		 * 数组
 		 */
 		int[] array= (int[])Ognl.getValue("new int[]{0,1,2,3,4,5}", context);
 		System.out.println(Arrays.toString(array));
@@ -84,7 +84,7 @@ public class OgnlTest2 {
 		Map map= (Map)Ognl.getValue("#{'key1':'value1','key2':'value2','key3':'value3'}", context);
 		System.out.println(map);
 		/**
-		 * ͶӰ��ѡ��
+		 * 放置List
 		 */
 		List<Teacher> teacherList=new ArrayList<Teacher>();
 		teacherList.add(new Teacher("Jack1",2000));
@@ -94,11 +94,11 @@ public class OgnlTest2 {
 		teacherList.add(new Teacher("Jack5",2800));
 		teacherList.add(new Teacher("Jack6",3000));
 		context.put("teacherList", teacherList);
-		//ͶӰ
+		//
 		System.out.println(Ognl.getValue("teacherList.{name}", context));
 		System.out.println(Ognl.getValue("teacherList.{#this.name.toUpperCase()}", context));
 		System.out.println(Ognl.getValue("teacherList.{#this.salary>2500?#this.salary:#this.salary+99}", context));
-		//ѡ��
+		//
 		System.out.println(Ognl.getValue("teacherList.{?#this.salary>2500}", context));
 		System.out.println(Ognl.getValue("teacherList.{?#this.salary>2500}[0]", context));
 		System.out.println(Ognl.getValue("teacherList.{^#this.salary>2500}", context));
