14d774bb2c4c0b30378b555bd01cdeb6bcd7f8fd
dbdu <1797890817@qq.com>
2017-02-22 16:40:41 +0800

    Mybatis34-Demo add 1--N
    Signed-off-by: dbdu <1797890817@qq.com>

--- Mybatis34-Demo/src_user/com/yihaomen/mybatis/inter/IUserOperation.java
+++ Mybatis34-Demo/src_user/com/yihaomen/mybatis/inter/IUserOperation.java
@@ -2,17 +2,20 @@ package com.yihaomen.mybatis.inter;
 
 import java.util.List;
 
+import com.yihaomen.mybatis.model.Article;
 import com.yihaomen.mybatis.model.User;
 
 public interface IUserOperation {
 	public void addUser(User user);
-	
+
 	public void updateUser(User user);
-	
+
 	public void deleteUser(int id);
-	
-	public List<User> selectUsers(String userName);   
-	
+
+	public List<User> selectUsers(String userName);
+
 	public User selectUserByID(int id);
+
+	public List<Article> getUserArticles(int id);
 
 }
