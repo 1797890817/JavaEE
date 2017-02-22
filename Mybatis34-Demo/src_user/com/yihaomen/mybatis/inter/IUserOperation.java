ac43c81b90a1d30fc6942a4d180f4bca60054072
dbdu <1797890817@qq.com>
2017-02-22 15:58:17 +0800

    Mybatis-34 ADD CRUD
    Signed-off-by: dbdu <1797890817@qq.com>

--- Mybatis34-Demo/src_user/com/yihaomen/mybatis/inter/IUserOperation.java
+++ Mybatis34-Demo/src_user/com/yihaomen/mybatis/inter/IUserOperation.java
@@ -1,8 +1,18 @@
 package com.yihaomen.mybatis.inter;
 
+import java.util.List;
+
 import com.yihaomen.mybatis.model.User;
 
 public interface IUserOperation {
+	public void addUser(User user);
+	
+	public void updateUser(User user);
+	
+	public void deleteUser(int id);
+	
+	public List<User> selectUsers(String userName);   
+	
 	public User selectUserByID(int id);
 
 }
--- Mybatis34-Demo/test_src/com/yihaomen/test/Test.java
+++ Mybatis34-Demo/test_src/com/yihaomen/test/Test.java
@@ -1,6 +1,7 @@
 package com.yihaomen.test;
 
 import java.io.Reader;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -27,16 +28,94 @@ public class Test {
 		return sqlSessionFactory;
 	}
 
-	public static void main(String[] args) {
+	/**
+     * 测试增加,增加后，必须提交事务，否则不会写入到数据库.
+     */
+    public void addUser(){
+        User user=new User();
+        user.setUserAddress("人民广场");
+        user.setUserName("飞鸟");
+        user.setUserAge(80+"");	//int ---> string
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            IUserOperation userOperation=session.getMapper(IUserOperation.class);
+            userOperation.addUser(user);
+            session.commit();
+            System.out.println("当前增加的用户 id为:"+user.getId());
+        } finally {
+            session.close();
+        }
+    }
+	
+    public void updateUser(){
+        //先得到用户,然后修改，提交。
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            IUserOperation userOperation=session.getMapper(IUserOperation.class);
+            User user = userOperation.selectUserByID(2);            
+            user.setUserAddress("原来是魔都的浦东创新园区");
+            userOperation.updateUser(user);
+            session.commit();
+
+        } finally {
+            session.close();
+        }
+    }
+    
+    /**
+     * 删除数据，删除一定要 commit.
+     * @param id
+     */
+    public void deleteUser(int id){
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            IUserOperation userOperation=session.getMapper(IUserOperation.class);          
+            userOperation.deleteUser(id);
+            session.commit();            
+        } finally {
+            session.close();
+        }
+    }
+    
+	public void getUserList(String userName) {
 		SqlSession session = sqlSessionFactory.openSession();
 		try {
-			//User user = (User) session.selectOne("com.yihaomen.mybatis.models.UserMapper.selectUserByID", 1);
-			IUserOperation userOperation=session.getMapper(IUserOperation.class);	//使用接口
-			User user = userOperation.selectUserByID(1);
-			System.out.println(user.getUserAddress());
-			System.out.println(user.getUserName());
+			IUserOperation userOperation = session.getMapper(IUserOperation.class);
+			List<User> users = userOperation.selectUsers(userName);
+			for (User user : users) {
+				System.out.println(user.getId() + ":" + user.getUserName() + ":" + user.getUserAddress());
+			}
 		} finally {
 			session.close();
 		}
+	}
+
+	public static void main(String[] args) {
+		/*SqlSession session = sqlSessionFactory.openSession();
+		try {
+			// User user = (User)
+			// session.selectOne("com.yihaomen.mybatis.models.UserMapper.selectUserByID",
+			// 1);
+			IUserOperation userOperation = session.getMapper(IUserOperation.class); // 使用接口
+			// 获取单个User
+			// User user = userOperation.selectUserByID(1);
+			// System.out.println(user.getUserAddress());
+			// System.out.println(user.getUserName());
+
+			
+		} finally {
+			session.close();
+		}*/
+		
+		Test testUser=new Test();
+		//Add User
+		//testUser.addUser();
+		//Update User
+		//testUser.updateUser();
+		//Delete User
+		testUser.deleteUser(2);
+		
+        testUser.getUserList("%");
+		
 	}
 }
