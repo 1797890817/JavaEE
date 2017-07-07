23378432a29bf71a135c8e036b7f80af9449b548
dbdu <1797890817@qq.com>
2017-07-08 06:44:05 +0800

    update sql file

--- SpringmvcSpringMybatis/src/main/webapp/pages/sql/SpringmvcSpringMybatis.sql
+++ SpringmvcSpringMybatis/src/main/webapp/pages/sql/SpringmvcSpringMybatis.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- 主机:                           192.168.43.20
--- 服务器版本:                        5.7.9 - MySQL Community Server (GPL)
+-- 主机:                           192.168.1.10
+-- 服务器版本:                        5.7.17-1 - (Debian)
 -- 服务器操作系统:                      Linux
--- HeidiSQL 版本:                  9.4.0.5127
+-- HeidiSQL 版本:                  9.4.0.5148
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -29,12 +29,31 @@ CREATE TABLE IF NOT EXISTS `smi_shop_info` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- 正在导出表  SpringmvcSpringMybatis.smi_shop_info 的数据：~1 rows (大约)
+-- 正在导出表  SpringmvcSpringMybatis.smi_shop_info 的数据：~0 rows (大约)
 DELETE FROM `smi_shop_info`;
 /*!40000 ALTER TABLE `smi_shop_info` DISABLE KEYS */;
 INSERT INTO `smi_shop_info` (`id`, `shop_no`, `shop_name`, `shop_type`, `shop_password`, `note`) VALUES
 	(1, '111111', 'shop-test', 'normal', '123456', 'test-data');
 /*!40000 ALTER TABLE `smi_shop_info` ENABLE KEYS */;
+
+-- 导出  表 SpringmvcSpringMybatis.student 结构
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stu_id` int(11) NOT NULL,
+  `stu_name` varchar(50) NOT NULL,
+  `stu_sex` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='學生組件表';
+
+-- 正在导出表  SpringmvcSpringMybatis.student 的数据：~3 rows (大约)
+DELETE FROM `student`;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` (`id`, `stu_id`, `stu_name`, `stu_sex`) VALUES
+	(9, 201677, 'FFF', 11),
+	(10, 20170708, '王五', 1),
+	(13, 20170320, '张三', 2);
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
